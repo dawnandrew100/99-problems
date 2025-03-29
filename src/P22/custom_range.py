8f2374505b88cc161f1e3a6d7564b5d41fac7ee0
Andrew Hennis <dawnandrew100@gmail.com>
2025-03-28 21:32:17 -0400

    Problem 22 solution
    There's already a range function in python so I attempted to also answer
    this question without using the range function or using a for loop
    (which would also necessitate the range function)

--- src/P22/custom_range.py
+++ src/P22/custom_range.py
@@ -1,8 +1,18 @@
 def main():
-    pass
+    start_value = 4
+    end_value = 9
+    print(list(range(start_value, end_value+1))) # actual answer
+    print(custom_range(start_value, end_value))  # answer without range or for loops
 
 def custom_range(start: int, end: int) -> list[int] | None:
-    if not isinstance(start, int) or not isinstance(end, int):
+    if not isinstance(start, int) or not isinstance(end, int) or end < start:
         return None
+    range_list = []
+    i = start
+    while i <= end:
+        range_list.append(i)
+        i += 1
+    return range_list
+
 if __name__ == "__main__":
     main()
